89204bd789a252606d69cfaa3a00c1c10416651b
Tom Danny Pedersen <tdp@northflow.no>
2026-06-24 17:22:23 +0200

    Norsk som standardsprak + ekte northflow.no-logo/favicon
    - DEFAULT_LOCALE = nb-NO (Umami har full norsk oversettelse)
    - Bruker den faktiske Northflow-logoen (northflow.no) som in-app-logo,
      hele favicon-settet og selvstendig icon.svg

--- src/components/svg/Logo.tsx
+++ src/components/svg/Logo.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 
-// Northflow-merke: navy avrundet firkant med hvit bowtie.
+// Northflow-logo: den faktiske merkevaren fra northflow.no (navy bowtie-badge).
 const SvgLogo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -9,9 +9,7 @@ const SvgLogo = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <rect width="24" height="24" rx="5" fill="#37425C" />
-    <path fill="#fff" d="M4.6 6 12 12 4.6 18Z" />
-    <path fill="#fff" d="M19.4 6 12 12 19.4 18Z" />
+    <image href="/northflow-logo.png" x="0" y="0" width="24" height="24" />
   </svg>
 );
 export default SvgLogo;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,7 +19,7 @@ export const FAVICON_URL = 'https://icons.duckduckgo.com/ip3/{{domain}}.ico';
 export const LINKS_URL = `${globalThis?.location?.origin}/q`;
 export const PIXELS_URL = `${globalThis?.location?.origin}/p`;
 
-export const DEFAULT_LOCALE = 'en-US';
+export const DEFAULT_LOCALE = 'nb-NO';
 export const DEFAULT_THEME = 'light';
 export const DEFAULT_ANIMATION_DURATION = 300;
 export const DEFAULT_DATE_RANGE_VALUE = '24hour';
